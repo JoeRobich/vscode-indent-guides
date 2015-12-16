45d856c6bf337b48634a5500074035ee497c2fbc
Joey Robichaud <joe.robich@gmail.com>
2015-12-16 09:17:32 -0500

    Add indent guides to lines of whitespace

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ class IndentGuideController {
         window.onDidChangeTextEditorSelection(guideDecorator.updateIndentGuides, guideDecorator, subscriptions);
         window.onDidChangeActiveTextEditor(guideDecorator.updateIndentGuides, guideDecorator, subscriptions);
         this._disposable = Disposable.from(...subscriptions);
-
+    
         guideDecorator.updateIndentGuides();
     }
 
@@ -33,26 +33,27 @@ class GuideDecorator {
         let editor = window.activeTextEditor;
         if (!editor)
             return;
-            
+        
         let lines:TextLine[] = this.getNonEmptyLines(editor.document);
            let ranges:Range[] = _(lines)
             .map(line => this.getGuideStops(line, editor.options.tabSize))
             .flatten<Range>()
             .value();
-            
+        
         editor.setDecorations(this._indentGuide, ranges);
     }
     
     getNonEmptyLines(document:TextDocument):TextLine[] {
         return _(_.range(0, document.lineCount))
             .map(lineNumber => document.lineAt(lineNumber))
-            .filter(line => !line.isEmptyOrWhitespace || line.firstNonWhitespaceCharacterIndex == 0)
+            .filter(line => line.firstNonWhitespaceCharacterIndex != 0)
             .value();
     }
     
     getGuideStops(line:TextLine, tabSize:number):Range[] {
         tabSize = line.text[0] === '\t' ? 1 : tabSize; // Currently expects the indentation to be either tabs or spaces. Produces strange output when both are used on the same line.
-        let depth = line.firstNonWhitespaceCharacterIndex / tabSize;
+        let indentation = line.isEmptyOrWhitespace ? line.text.length : line.firstNonWhitespaceCharacterIndex;
+        let depth = indentation / tabSize;
         return _(_.range(1, depth))
             .map(step => new Position(line.lineNumber, step * tabSize))
             .map(position => new Range(position, position))
